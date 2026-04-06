e86034a883ea2bad3d4f040fe0aa4c79954675f7
mamun-coderman <mamun.coderman@gmail.com>
2026-04-06 23:56:34 +0600

    Temperature converter programme added

---
+++ variable&datatype/temperatureConverter.js
@@ -0,0 +1,28 @@
+/* 
+Problem:
+
+Write a function that takes a temperature value as input along with its unit and converts it to the opposite unit.
+
+If the input is in Celsius (C), convert it to Fi track Fahrenheit (F)
+If the input is in Fahrenheit (F), convert it to Celsius (C)
+
+*/
+// Code:
+
+function convertTemperature(input){
+    const value = parseFloat(input);
+    const unit = input.slice(-1).toUpperCase();
+    if (unit === "C"){
+        const fahrenheit = ( value * 9/5 ) + 32;
+        return `${value}°C = ${fahrenheit}°F`;
+    }
+    else if ( unit === "F" ) {
+        const celsius = ( fahrenheit - 32 ) * 5/9;
+        return `${value}°F = ${celsius}`;
+    }
+    else {
+        return `Invalid input ! Type C for celcius and F for fahrenheit`
+    }
+}
+
+console.log(convertTemperature("26C"));
