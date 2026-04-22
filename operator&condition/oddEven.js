fac1930dc879506ec09730e89f8b4611d0e29ed3
mamun-coderman <mamun.coderman@gmail.com>
2026-04-22 22:37:42 +0600

    odd even checking program added

---
+++ operator&condition/oddEven.js
@@ -0,0 +1,20 @@
+// Problem: Write code to check if a number is even or odd 
+
+// code: 
+
+/*
+
+function checkingOddEven(inputNumber) {
+    if (inputNumber % 2 === 0) {
+        console.log("Even")
+    }
+    else {
+        console.log("Odd")
+    }
+}
+
+const value = 11;
+
+console.log(checkingOddEven(value))
+
+*/
