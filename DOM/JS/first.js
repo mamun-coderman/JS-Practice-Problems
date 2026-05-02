d2fc2242d1327067cba2b879f93afb5852269a5d
mamun-coderman <mamun.coderman@gmail.com>
2026-05-02 18:52:20 +0600

    changed environment by clicking button dom program added

--- DOM/JS/first.js
+++ DOM/JS/first.js
@@ -1,1 +1,11 @@
 console.log("This is from first js file.")
+
+const btn = document.getElementById("text-btn");
+btn.addEventListener("click", function(){ 
+    btn.style.backgroundColor = "orange"
+    btn.style.color = "white"
+    btn.innerText = "changed"
+    const btnTitle = document.getElementById("btn-title");
+    btnTitle.innerText = "Hello, I am changed from first JS file."
+    
+})
