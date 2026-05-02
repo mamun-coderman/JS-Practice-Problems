599780c2c5fb4743a9eb96e8fee2518ac4e0aad9
mamun-coderman <mamun.coderman@gmail.com>
2026-05-02 21:05:24 +0600

    button click counter DOM program added

--- DOM/JS/first.js
+++ DOM/JS/first.js
@@ -1,5 +1,7 @@
+// print this line beacause of detecting exact js file.
 console.log("This is from first js file.")
 
+// essential js code for html first section starts here
 const btn = document.getElementById("text-btn");
 btn.addEventListener("click", function(){ 
     btn.style.backgroundColor = "orange"
@@ -7,5 +9,25 @@ btn.addEventListener("click", function(){
     btn.innerText = "changed"
     const btnTitle = document.getElementById("btn-title");
     btnTitle.innerText = "Hello, I am changed from first JS file."
-    
+    const backgroundChange = document.getElementById("first-section");
+    backgroundChange.style.backgroundColor = "skyblue";
 })
+
+// first section ends here
+// here, when we click on button, the background of section and button will change. The number increases per click .
+
+let count = 0;
+
+const numBtn = document.getElementById("num-btn");
+numBtn.addEventListener("click", function(){ 
+    numBtn.style.backgroundColor = "orange";
+    numBtn.style.color = "white";
+    document.getElementById("second-section").style.backgroundColor = "skyblue";
+    const displayNum = document.getElementById("count");
+
+    count += 1;
+    displayNum.innerText = count;
+
+})
+
+// second section ends here
