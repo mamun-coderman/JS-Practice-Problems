b09f58fcdff4c4043df17154f9a9d41b2c73091c
mamun-coderman <mamun.coderman@gmail.com>
2026-05-17 21:37:36 +0600

    digit count in a number program added

---
+++ loop/counDigit.js
@@ -0,0 +1,7 @@
+// Count how many digits are in a number
+
+const number = 12457;
+const digitsOfNumber = number.toString().length;
+
+console.log(`Total digits of given number is ${digitsOfNumber}`)
+
